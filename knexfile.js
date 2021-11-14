c1119e6a4373b9e72d4b2ace18a214e9d6d5ceb1
Thanh Nguyen <vnscriptkid@gmail.com>
2021-11-14 20:13:22 +0700

    chore: setup again

--- knexfile.js
+++ knexfile.js
@@ -1,27 +1,27 @@
 // Update with your config settings.
 var options = {
-    development: {
-        client: 'pg',
-        connection: 'postgres://localhost/knex_video',
-        migrations: {
-            directory: __dirname + '/db/migrations',
-          },
-        seeds: {
-            directory: __dirname + '/db/seeds',
-          },
-      },
-    production: {
-        client: 'pg',
-        connection: process.env.DATABASE_URL,
-        migrations: {
-            directory: __dirname + '/db/migrations',
-          },
-        seeds: {
-            directory: __dirname + '/db/seeds/production',
-          },
-      },
-  };
+  development: {
+    client: "pg",
+    connection: "postgresql://postgres:123456@localhost:5432/knex_todos",
+    migrations: {
+      directory: __dirname + "/db/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds",
+    },
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + "/db/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds/production",
+    },
+  },
+};
 
-var environment = process.env.NODE_ENV || 'development';
+var environment = process.env.NODE_ENV || "development";
 var config = options[environment];
-module.exports = require('knex')(config);
+module.exports = config;
